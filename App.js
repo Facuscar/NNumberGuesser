c6b7e6cbda43221648b73a4e18eecc016b9e4f21
facuscar <facuscardillo@gmail.com>
2024-09-18 23:32:29 -0300

    Add background color

--- App.js
+++ App.js
@@ -5,9 +5,15 @@ import StartGameScreen from './screens/StarGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <View style={styles.screen}>
+      <StartGameScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: "#ddb52f",
+    flex: 1,
+  }
 });
